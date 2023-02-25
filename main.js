bbfd947e02ead33a12511be9d918c9285ab376c1
Fury-AF <99542261+Fury-AF@users.noreply.github.com>
2023-02-25 08:05:00 +0100

    Kommit

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ window.addEventListener('load', init)
 const Kepek = []
 const Tortenet =[]
 function init() {
+console.log(document.querySelector("img"));
+document.querySelector("img").addEventListener("img", kiir);
+}
 
+function kiir(){
+console.log("Hello")
 }
